eac63170acb7e796344e3872f638cdebea796120
Aniket Rai <prosniperaniket@gmail.com>
2023-01-28 12:39:20 +0530

    Update EvController.java

--- src/main/java/com/aniket/app/controller/EvController.java
+++ src/main/java/com/aniket/app/controller/EvController.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aniket.app.model.evStation;
@@ -24,9 +28,29 @@ public class EvController {
 	private evRepo evRepo;
 
 	@GetMapping("/")
-	public ResponseEntity<List<evStation>> getAll() {
+	public ResponseEntity<List<evStation>> getAll(@RequestParam(required = false) Integer limit,
+			@RequestParam(required = false) Integer offset, @RequestParam(required = false) String sort,
+			@RequestParam(required = false) String param) {
 		try {
-			return new ResponseEntity<>(evRepo.findAll(), HttpStatus.OK);
+			List<evStation> res = null;
+			Sort sortob = null;
+			if (sort != null && param != null) {
+				sortob = Sort.by(sort.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC, param);
+
+			}
+
+			if (limit != null) {
+				if (sortob == null) {
+					Page<evStation> res1 = evRepo.findAll(PageRequest.of(offset == null ? 0 : offset, limit));
+					res = res1.getContent();
+				} else {
+					Page<evStation> res1 = evRepo.findAll(PageRequest.of(offset == null ? 0 : offset, limit, sortob));
+					res = res1.getContent();
+				}
+			} else {
+				res = evRepo.findAll();
+			}
+			return new ResponseEntity<>(res, HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<>(null, HttpStatus.SERVICE_UNAVAILABLE);
